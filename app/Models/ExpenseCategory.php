3a92112a8cbc4b95a100574337aeb74ed4daf4c2
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-03-24 12:38:25 +0545

    jsut

--- app/Models/ExpenseCategory.php
+++ app/Models/ExpenseCategory.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\ExpenseItem;
+use App\Models\ExpenseRecordItem;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -17,4 +18,16 @@ class ExpenseCategory extends Model
     {
         return $this->hasMany(ExpenseItem::class);
     }
+
+    public function expense_record_items(): \Illuminate\Database\Eloquent\Relations\HasManyThrough
+{
+    return $this->hasManyThrough(
+        ExpenseRecordItem::class, 
+        ExpenseItem::class,
+        'expense_category_id', // Foreign key on ExpenseItem table
+        'expense_item_id',     // Foreign key on ExpenseRecordItem table
+        'id',                  // Local key on ExpenseCategory table
+        'id'                   // Local key on ExpenseItem table
+    );
+}
 }
